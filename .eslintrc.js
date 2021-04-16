8f2b56626b9ec60b4ea73aaf452af501402d2947
wearedprompt <wearedprompt@gmail.com>
2021-04-17 03:28:27 +0700

    Edit Script without typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2021,
     sourceType: 'module',
   },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['prettier', 'plugin:prettier/recommended'],
   rules: {},
 }
